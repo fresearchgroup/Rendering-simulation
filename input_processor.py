3a440e5ae92f87cdc0dfc8e7353701a5a8c6e92e
makrandr1999 <makrand.r@somaiya.edu>
2019-07-12 17:16:06 +0530

    create a file directory to store rendered output files

--- input_processor.py
+++ input_processor.py
@@ -2,9 +2,10 @@ import os
 import sys
 import subprocess
 from core.generators import *
+import shutil
 from pylatex import Document, Section,Subsection, Itemize,Tabular,Figure
 import xml.etree.ElementTree as ET
-
+xcos_file_name=''
 def process_file(xml_filepath):
     block_list= []
     context= []
@@ -15,8 +16,11 @@ def process_file(xml_filepath):
         print("Couldn't open file.Please check if the file path is correct.")
         exit()
 
-    #opening the file in Scilab 6.0 to port xml schema to the latest version.
-    path_to_upgraded_file=xml_filepath.split('/')[-1].split('.')[0]+'_updated.xcos'
+    #opening the file in Scilab 6.0 to port xml schema to the latest version and create a directory to store the output file.
+    xcos_file_name=xml_filepath.split('/')[-1].split('.')[0]
+    if not os.path.exists(xcos_file_name):
+        os.mkdir(xcos_file_name)
+    path_to_upgraded_file=xcos_file_name+'_updated.xcos'
     command='scs_m = xcosDiagramToScilab('+'\''+xml_filepath+'\''+');xcosDiagramToScilab("'+path_to_upgraded_file+'", scs_m); quit();'
     subprocess.call(['scilab-adv-cli', '-e', command])
 
@@ -40,7 +44,7 @@ def process_file(xml_filepath):
         for data in root.findall('./Array/add'):
             context.append(data.get('value'))
 
-        return [block_list,context]
+        return [block_list,context,xcos_file_name]
 
     except NameError:
         print("Block not found:", funcname)
@@ -55,17 +59,21 @@ def process_file(xml_filepath):
 
 
 
-def render(img_dir, img_name, pdf_name , output_list):
-    image_filename = os.path.join(img_dir,img_name)
+def render(img_filepath,output_list):
+    image_filename = img_filepath.split('/')[-1]
+    cwd = os.getcwd()+'/'+output_list[2]
+    shutil.copy2(img_filepath,cwd)
+
+    #print(image_filename)
+
     geometry_options = {"tmargin": "1cm","lmargin":"1cm" }
     doc = Document(geometry_options=geometry_options)
     #creates image section in LaTeX
+
     with doc.create(Section("Xcos")):
         with doc.create(Figure(position='h!')) as abs_pic:
             abs_pic.add_image(image_filename, width='450px')
-            doc.append('\n')
-            doc.append('\n')
-            doc.append('\n')
+            doc.append("\n\n\n")
     #creates Context section in LaTeX if context exists
     if output_list[1]:
         with doc.create(Subsection("Context")):
@@ -76,9 +84,7 @@ def render(img_dir, img_name, pdf_name , output_list):
     with doc.create(Subsection("Blocks")):
         for i in output_list[0]:
             if i is not None:
-                doc.append('\n')
-                doc.append('\n')
-                doc.append('\n')
+                doc.append("\n\n\n")
                 with doc.create(Tabular('|c|c|')) as table:
                     table.add_hline()
                     for key, value in i.parameters().items():
@@ -86,10 +92,11 @@ def render(img_dir, img_name, pdf_name , output_list):
                         table.add_hline()
 
 
+    file_path= output_list[2]+'/'+output_list[2]
+    doc.generate_pdf(file_path, clean_tex=False)
 
-    doc.generate_pdf(pdf_name, clean_tex=False)
 
 if __name__ == '__main__':
     output_list=process_file(sys.argv[1])
-    render(sys.argv[2], sys.argv[3],sys.argv[4], output_list)
+    render(sys.argv[2],output_list)
 
